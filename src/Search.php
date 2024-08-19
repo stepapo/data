80b80b092790fcf02ffd40afdc724e976965dc1e
Štepán Postler <stepan.postler@gmail.com>
2024-08-19 11:43:16 +0200

    wip

--- src/Search.php
+++ src/Search.php
@@ -14,8 +14,9 @@ class Search extends Schematic
 	public ?string $placeholder = null;
 	/** @var callable */ public mixed $prepareCallback = null;
 	/** @var callable */ public mixed $suggestCallback = null;
-	public string $sortDirection = ICollection::ASC;
-	#[Type(OrmFunction::class)] public OrmFunction|array $searchFunction;
+	/** @var callable */ public mixed $searchCallback = null;
+	#[Type(OrmFunction::class)] public OrmFunction|array|null $searchFunction;
 	#[Type(OrmFunction::class)] public OrmFunction|array|null $sortFunction = null;
+	public string $sortDirection = ICollection::ASC;
 	public bool $hide = false;
 }
