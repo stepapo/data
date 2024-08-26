31babaa98827ff481fc704a63bff486a98392b40
Štepán Postler <stepan.postler@gmail.com>
2024-08-26 17:08:55 +0200

    wip

--- src/Chart.php
+++ src/Chart.php
@@ -9,23 +9,24 @@ use Stepapo\Utils\Schematic;
 
 class Chart extends Schematic
 {
-    public const string CHART_TYPE_LINE = 'LineChart';
-    public const string CHART_TYPE_AREA = 'AreaChart';
-    public const string CHART_TYPE_PIE = 'PieChart';
-    public const string CHART_TYPE_BAR = 'BarChart';
+	public const string CHART_TYPE_LINE = 'LineChart';
+	public const string CHART_TYPE_AREA = 'AreaChart';
+	public const string CHART_TYPE_PIE = 'PieChart';
+	public const string CHART_TYPE_BAR = 'BarChart';
 
-    public ?string $type = null;
-    public ?float $min = null;
-    public ?float $max = null;
-    public ?int $left = null;
-    public ?int $right = null;
-    public ?int $top = null;
-    public ?int $bottom = null;
-    public ?string $hFormat = null;
-    public ?string $vFormat = null;
-    public bool $cumulative = false;
-    public ?int $base = 0;
-    public array $series = [];
-    public ?string $stacked = null;
-    public array $colors = [];
+	public ?string $type = null;
+	public ?float $min = null;
+	public ?float $max = null;
+	public ?int $left = null;
+	public ?int $right = null;
+	public ?int $top = null;
+	public ?int $bottom = null;
+	public ?string $hFormat = null;
+	public ?string $vFormat = null;
+	public bool $cumulative = false;
+	public ?int $base = 0;
+	public array $series = [];
+	public bool $separateSeriesAxis = false;
+	public ?string $stacked = null;
+	public array $colors = [];
 }
