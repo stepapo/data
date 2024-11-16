d1f7cba4303beb9776962c0fc3f03e2fcf307b0f
Štepán Postler <stepan.postler@fsv.cvut.cz>
2024-11-16 16:44:44 +0100

    wip

--- src/Control/FilterList/FilterListControl.php
+++ src/Control/FilterList/FilterListControl.php
@@ -31,14 +31,10 @@ class FilterListControl extends DataControl
 	public function render(): void
 	{
 		$this->template->show = false;
-		$filterCount = 0;
 		foreach ($this->columns as $column) {
 			if ($column->filter && !$column->filter->hide) {
-				$filterCount++;
-				if ($filterCount > 1) {
-					$this->template->show = true;
-					break;
-				}
+				$this->template->show = true;
+				break;
 			}
 		}
 		$this->template->columns = $this->columns;
