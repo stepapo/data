8b95535a2416d11d7ea9f00ab4da29a230ecb04e
Štepán Postler <stepan.postler@fsv.cvut.cz>
2024-12-14 14:56:06 +0100

    wip

--- src/Link.php
+++ src/Link.php
@@ -1,8 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Stepapo\Data;
 
-class Link
+use Stepapo\Utils\Schematic;
+
+
+class Link extends Schematic
 {
 	public string $destination;
 	public array $args = [];
