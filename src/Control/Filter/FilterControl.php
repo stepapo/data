5ee1922543f8a4c276a63ea02ce75c5de29bd1a2
Štepán Postler <stepan.postler@gmail.com>
2025-07-25 12:29:03 +0200

    wip

--- src/Control/Filter/FilterControl.php
+++ src/Control/Filter/FilterControl.php
@@ -33,8 +33,9 @@ class FilterControl extends DataControl
 	public function loadState(array $params): void
 	{
 		parent::loadState($params);
+		bdump($this->column->filter);
 		$this->value = $this->column->filter->value ?: ($this->value ?: $this->column->filter->defaultValue);
-		$this->options = $this->column->filter->options ?: ($this->column->filter->selectedCallback)($this->value);
+		$this->options = $this->column->filter->options ?: ($this->column->filter->selectedCallback ? ($this->column->filter->selectedCallback)($this->value) : []);
 	}
 
 
