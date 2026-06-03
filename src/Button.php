f9c009ed737236ea7c403020b39d0d9b7ec542e4
Štepán Postler <stepan.postler@fsv.cvut.cz>
2026-06-03 15:32:24 +0200

    wip

---
+++ src/Button.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stepapo\Data;
+
+use Stepapo\Utils\Attribute\KeyProperty;
+use Stepapo\Utils\Config;
+
+
+class Button extends Config
+{
+	#[KeyProperty] public string $name;
+	#[ValueProperty] public string $label;
+	public string $style = 'primary';
+	public ?\Closure $callback = null;
+}
